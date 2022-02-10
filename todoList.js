2159e45b00fafa2f090a72f9784875eba04c98e0
bhagitha <bhagithaparamesh@gmail.com>
2022-02-10 09:24:56 +0530

    Initial commit

---
+++ todoList.js
@@ -0,0 +1,39 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var response = JSON.parse(this.responseText);
+        var output = '';
+        for (var i = 0; i < response.length; i++) {
+            if ((response[i].completed) == true) {
+                output += `<input id="check1" type="checkbox"  checked="checked" disabled style="";>
+            <label style="margin-left:5px;">${response[i].title}</label><br><hr>`;
+            } else {
+                output += `<input type="checkbox" onclick="checked_counter(this)" >
+                <label  style="margin-left:5px;">${response[i].title}</label><br><hr>`;
+            }
+        }
+        document.getElementById("main").innerHTML = `${output}`;
+    }
+};
+xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+xhttp.send();
+
+var counter = 0;
+
+function checked_counter(value) {
+console.log(value.checked)
+    var promis = new Promise(function(resolve, reject) {
+        if (value.checked == true) {
+            counter++;
+            if (counter == 5) {
+                resolve(" 5 tasks completed!! ");
+            }
+        } else {
+            counter--;
+        }
+    });
+    promis.then(function(s) {
+        alert(s);
+    });
+
+}
